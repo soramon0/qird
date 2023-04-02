1ef59ca0d02489737062c86f9a27bf3fbe85ec1f
soramon0 <23435312+soramon0@users.noreply.github.com>
2023-04-02 22:39:06 +0000

    add support for more operators

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -13,8 +13,9 @@ let add = fn(x, y) {
 x + y;
 };
 let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
 `
-	l := New(input)
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -56,9 +57,22 @@ let result = add(five, ten);
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
+	l := New(input)
 	for i, tt := range tests {
 		tok := l.NextToken()
 		if tok.Type != tt.expectedType {
