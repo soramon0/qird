221c5361dc2a97bbddd3380abf62398da7ce6bdc
soramon0 <23435312+soramon0@users.noreply.github.com>
2023-04-02 22:45:02 +0000

    add new keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -9,12 +9,21 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 let ten = 10;
+
 let add = fn(x, y) {
 x + y;
 };
+
 let result = add(five, ten);
+
 !-/*5;
 5 < 10 > 5;
+
+if (5 < 10) {
+return true;
+} else {
+return false;
+}
 `
 
 	tests := []struct {
@@ -69,6 +78,23 @@ let result = add(five, ten);
 		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
